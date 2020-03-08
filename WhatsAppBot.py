ad5956d1997b82b40b914931c2ac259842d33ab0
akhileshsingh <trycatch55@gmail.com>
2020-03-08 11:44:38 +0530

    Script is capable to send messages to multiple users.

--- WhatsAppBot.py
+++ WhatsAppBot.py
@@ -8,13 +8,14 @@ chrome_browser.get('https://web.whatsapp.com/')
 
 time.sleep(15)
 
-user_name = 'My'
+user_name_list = ['WhatsApp Bot', 'My']
 
-user = chrome_browser.find_element_by_xpath('//span[@title="{}"]'.format(user_name))
-user.click()
+for user_name in user_name_list:
+    user = chrome_browser.find_element_by_xpath('//span[@title="{}"]'.format(user_name))
+    user.click()
 
-message_box = chrome_browser.find_element_by_xpath('//div[@class="_13mgZ"]')
-message_box.send_keys('Hey, I am your whatsapp bot')
+    message_box = chrome_browser.find_element_by_xpath('//div[@class="_13mgZ"]')
+    message_box.send_keys('Hey, I am your whatsapp bot')
 
-message_box = chrome_browser.find_element_by_xpath('//button[@class="_3M-N-"]')
-message_box.click()
+    message_box = chrome_browser.find_element_by_xpath('//button[@class="_3M-N-"]')
+    message_box.click()
